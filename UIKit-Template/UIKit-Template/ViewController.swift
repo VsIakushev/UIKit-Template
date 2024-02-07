99ce5ab903e53a35192ee11b168096432c38d7e7
VsIakushev <vsiakushev@gmail.com>
2024-02-06 18:28:41 -0800

    Подготовил шаблон для дальнейших заданий, создал ветку develop. SwiftFormat и SwiftLint настроены и работают, ошибок нет.

---
+++ UIKit-Template/UIKit-Template/ViewController.swift
@@ -0,0 +1,10 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        // super.viewDidLoad()
+    }
+}
