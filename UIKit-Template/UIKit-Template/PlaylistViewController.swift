c98d19652cae7380110f1662afe6e63db9bf2f59
VsIakushev <vsiakushev@gmail.com>
2024-02-11 19:19:08 -0800

    Сделал передачу данных на второй экран

--- UIKit-Template/UIKit-Template/PlaylistViewController.swift
+++ UIKit-Template/UIKit-Template/PlaylistViewController.swift
@@ -52,4 +52,26 @@ class PlaylistViewController: UIViewController {
 //
 //        navigationController?.pushViewController(playerVC, animated: true)
     }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Проверяем, что идентификатор перехода совпадает с тем, который вы указали в Interface Builder
+        var activeTrack = 0
+        if let button = sender as? UIButton {
+            switch button.titleLabel?.text {
+            case "Let it be": activeTrack = 0
+            case "Yesterday": activeTrack = 1
+            case "The Show Must Go On": activeTrack = 2
+            default: break
+            }
+        }
+
+        if segue.identifier == "toPlayerViewController" {
+            // Получаем ссылку на экран, на который происходит переход
+            if let destinationVC = segue.destination as? PlayerViewController {
+                // Передаем массив треков на второй экран
+                destinationVC.tracks = tracks
+                destinationVC.activeTrackIndex = activeTrack
+            }
+        }
+    }
 }
